499bf5c9fcd1ba8bf57715e63c2b1115675497bc
Noah <noaheasley408@gmail.com>
2020-12-07 16:25:04 -0600

    Made a number picking game

--- C++_Trial/Main.cpp
+++ C++_Trial/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+
 //void FizzBuzz(int num)
 //{
 //	for (int i = 0; i <= num; i++)
@@ -24,25 +25,66 @@ int main()
 	
 	//for (int i = 0; i <= 100; i+=2)
 	//FizzBuzz(num);
-	int num = 0;
+	
 	
 	std::cout << "Think of a number between 1 - 10 for me to guess" << std::endl;
-	 
-	int numbers[10];
+	
+	
+	int length = 10;
 	int min = 1;
 	int max = 10;
-	int goal = 5;
+
+	
 
 	while (min <= max)
 	{
+		int num = 0;
+		int higher = 1;
+		bool lower = 2;
+		bool correct = 3;
 		int guess = (min + max) / 2;
 
-		if (numbers[guess] == goal)
+		std::cout << "Is this your number? " << guess << std::endl;
+
+		std::cout << "1. Lower" << std::endl;
+		std::cout << "2. Higher" << std::endl;
+		std::cout << "3. Correct" << std::endl;
+
+
+		
+		std::cin >> num;
+		
+		
+		if (num == 1)
+		{
+			if (min == max)
+			{
+				std::cout << "liar -_-" << std::endl;
+			}
+			max = guess - 1;
+
+		}
+		else if (num == 2)
+		{
+			if (min == max)
+			{
+				std::cout << "liar -_-" << std::endl;
+			}
+			min = guess + 1;
+
+		}
+		else if (num == 3)
+		{
+			std::cout << "Yay :D" << std::endl;
+			break;
+		}
+		
+		/*if (numbers[guess] == goal)
 			return guess;
 		else if (numbers[guess] < goal)
 			min = guess + 1;
 		else if (numbers[guess] > goal)
-			max = guess - 1;
+			max = guess - 1;*/
 
 	}
 
