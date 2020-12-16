490ba1235d411512107edd484e2ea76d344f8417
Noah <Noaheasley408@gmail.com>
2020-12-16 16:56:07 -0600

    added stuff for characters

---
+++ Pointers/Game.cpp
@@ -0,0 +1,25 @@
+#include "Game.h"
+
+#include <iostream>
+
+void Game::start()
+{
+	Character* m_player1 = new Character(10, 10);
+	Character* m_player2 = new Character(10, 10);
+}
+
+void Game::update()
+{
+	m_player1->attack(m_player2);
+}
+
+void Game::draw()
+{
+	std::cout << "Player2 health is : " << m_player2->getHealth() << std::endl;
+}
+
+void Game::end()
+{
+	delete m_player1;
+	delete m_player2;
+}
