6919ae418f06558be2f801c934a5c57db7652e6e
Noah <noaheasley408@gmail.com>
2020-12-18 11:56:39 -0600

    allowed file.io to run

--- File.io/Main.cpp
+++ File.io/Main.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
 #include <fstream>
+#include "Character.h"
 
 int main()
 {
+	Character player = Character();
+	player.health = 100;
+	player.damage = 5;
+
+	//Example of saving to a text file
+
+	//Create an instance of a file stream
+	std::fstream file;
+
+	//Search for the file with the name given in the first
+	//argument. If no file with this name exists, one with
+	//the name is created. Then "std::ios::out" is used to
+	//say that we want to output data to the text file.
+	file.open("save.txt", std::ios::out | std::ios::_Nocreate);
+
+	//Check is the file isn't opened. If so, return from main.
+	if (!file.is_open())
+	{
+		return 1;
+	}
+
+	//Write player stats to the text file.
+	file << player.health << std::endl;
+	file << player.damage;
+
+	//Close the file when we're done.
+	file.close();
+
+
+	//Example of loading from a text file.
+	Character player2 = Character();
+	
+	//Search for a file that has the name given in the first
+	//argument. The "std::ios::in" argument is used to say 
+	//that we want to get input from the file.
+	file.open("save.txt", std::ios::in);
+
+	//Check is the file isn't opened. If so, return from main.
+	if (!file.is_open())
+	{
+		return 1;
+	}
+
+	//Reads the player stats from the text file.
+	file >> player2.health;
+	file >> player2.damage;
+
+	//Closes the file once we're done
+	file.close();
+
+	std::cout << player2.health << std::endl;
+	std::cout << player2.damage << std::endl;
 	return 0;
 }
--- Pointers/Game.cpp
+++ Pointers/Game.cpp
@@ -1,6 +1,8 @@
 #include "Game.h"
+#include <fstream>
 #include <iostream>
 
+std::fstream file;
 void Game::run()
 {
 	start();
@@ -20,6 +22,21 @@ void Game::start()
 {
 	Character* m_player1 = new Character(10, 10);
 	Character* m_player2 = new Character(10, 10);
+
+	file.open("save.txt", std::ios::out | std::ios::_Nocreate);
+
+	if (!file.is_open())
+	{
+		return;
+	}
+
+	file << m_player1->getHealth() << std::endl;
+	file << m_player1->getDamage() << std::endl;
+	file << m_player2->getHealth() << std::endl;
+	file << m_player2->getDamage();
+	file.close();
+	Character* m_player1 = new Character(m_player1->getHealth(), m_player1->getDamage());
+	Character* m_player2 = new Character(m_player2->getHealth(), m_player2->getDamage());
 	system("pause");
 }
 
@@ -28,6 +45,14 @@ void Game::update()
 	std::cout << "Player1's turn" << std::endl;
 	m_player1->attack(m_player2);
 	m_player2->heal(m_player2);
+
+	file.open("save.txt", std::ios::in);
+	if (!file.is_open())
+	{
+		return;
+	}
+	file >> m_player1->getHealth();
+	file >> player2.damage;
 }
 
 void Game::draw()
